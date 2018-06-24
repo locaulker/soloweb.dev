258b2b2ccb487286a36569f8ab9a48d0b4d1e3ac
Lawrence Caulker <locaulker@gmail.com>
2018-06-23 23:36:35 -0400

    Team section started. Owl carousel installed and initialled

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ $(window).on('load', function(){
   $('#status').fadeOut();
   $('#preloader').delay(350).fadeOut('slow');
 });
+
+
+/* *********************************************
+Owl-Carousel: Team Members
+********************************************* */
+$(function(){
+  // $(".owl-carousel").owlCarousel();
+  $("#team-members").owlCarousel({
+    items: 2,
+    margin: 10,
+    // loop: true,
+    rewind: true,
+    autoplay: true,
+    smartSpeed: 700,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    // navText: ['&larr;','&rarr;'],
+    navText: ['<i class="fa fa-angle-left fa-3x"></i>','<i class="fa fa-angle-right fa-3x"></i>']
+  });
+});
